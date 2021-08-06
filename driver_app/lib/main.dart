6eb6b21893d157af9685a5dc2aac9c696e8c9344
Avishka <mailavishka@gmail.com>
2021-08-06 13:08:55 +0530

    changing app name

--- driver_app/lib/main.dart
+++ driver_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RideX Driver APP',
+      title: 'RideX Driver',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
