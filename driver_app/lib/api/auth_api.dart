96b49de921c0b486be58e457c2ca8bf127834534
Avishka <mailavishka@gmail.com>
2021-09-24 20:05:27 +0530

    adding modals / utils

--- driver_app/lib/api/auth_api.dart
+++ driver_app/lib/api/auth_api.dart
@@ -1,1 +1,11 @@
+import 'package:driver_app/api/utils.dart';
 
+Future<void> signUpRequest({required String phone}) async {
+  String url = 'http://ridex.ml/api/auth/passenger/phoneAuth';
+  await postRequest(
+    urlOld: url,
+    data: {
+      "phone": phone,
+    },
+  );
+}
--- driver_app/lib/utils/bottom_nav_bar.dart
+++ driver_app/lib/utils/bottom_nav_bar.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:driver_app/pages/home/home_screens.dart';
+import 'package:driver_app/pages/profile/pages/profile_screen.dart';
+import 'package:driver_app/pages/tirp_history/pages/trip_history_screen.dart';
 
+final List<Widget> pages = [
+  // ProfileScreen(),
+  // HomeScreens(),
+  // TripHistoryScreen(),
+];
+
+Route createRoute(index) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => pages[index],
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
