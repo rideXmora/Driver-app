c42e45f8db70c9e32c52a84c26f1bb2e1556495c
Avishka <mailavishka@gmail.com>
2021-09-30 08:55:39 +0530

    adding api util / adding base url to aconfig

--- driver_app/lib/api/auth_api.dart
+++ driver_app/lib/api/auth_api.dart
@@ -3,9 +3,10 @@ import 'package:driver_app/api/utils.dart';
 Future<void> signUpRequest({required String phone}) async {
   String url = 'http://ridex.ml/api/auth/passenger/phoneAuth';
   await postRequest(
-    urlOld: url,
+    url: url,
     data: {
       "phone": phone,
     },
+    token: '',
   );
 }
