a0b773eaaa8eeb35311786ae7fa829eab662eb4f
Avishka <mailavishka@gmail.com>
2021-10-01 08:55:17 +0530

    adding submit otp

--- driver_app/lib/controllers/auth_controller.dart
+++ driver_app/lib/controllers/auth_controller.dart
@@ -1,7 +1,12 @@
+import 'package:driver_app/controllers/user_controller.dart';
+import 'package:driver_app/pages/bottom_navigation_bar_handler.dart';
 import 'package:driver_app/pages/sign_in_up/pages/language_selection_screen.dart';
 import 'package:driver_app/pages/sign_in_up/pages/mobile_number_verification_screen.dart';
+import 'package:driver_app/pages/sign_in_up/pages/registration/selecting_vchicle_type.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:driver_app/api/auth_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController extends GetxController {
   // SplashScreen data loading
@@ -15,6 +20,7 @@ class AuthController extends GetxController {
   Future<void> getOTP({required String phone, required String from}) async {
     if (phone.length == 12) {
       dynamic response = await phoneAuth(phone: phone);
+      debugPrint(response.toString());
       if (response["error"] != true) {
         if (from == "main") {
           Get.to(() => MobileNumberVerificationScreen(
@@ -29,6 +35,48 @@ class AuthController extends GetxController {
     } else {
       Get.snackbar("Phone number is not valid!!!",
           "Phone number must have 9 characters");
+    }
+  }
+
+  // submit otp from MobileNumberVerification
+  Future<void> submitOTP({required String phone, required String otp}) async {
+    SharedPreferences store = await SharedPreferences.getInstance();
+    debugPrint(otp.toString());
+
+    if (phone.length < 12) {
+      Get.snackbar("Phone number is not valid!!!",
+          "Phone number must have 9 characters");
+    } else if (otp.length < 6) {
+      Get.snackbar("OTP is not valid!!!", "Otp must have 6 characters");
+    } else {
+      dynamic response = await phoneVerify(phone: phone, otp: otp);
+
+      debugPrint(response["enabled"].toString());
+
+      if (!response["error"]) {
+        String token = response["body"]["token"];
+        String refreshToken = response["body"]["refreshToken"];
+        store.setString(
+          "token",
+          token,
+        );
+        store.setString(
+          "refreshToken",
+          refreshToken,
+        );
+        if (response["body"]["enabled"]) {
+          Get.find<UserController>().saveDriverData(response["body"]);
+          debugPrint("user exist : " +
+              Get.find<UserController>().passenger.value.toString());
+          Get.offAll(() => BottomNavHandler());
+        } else {
+          Get.find<UserController>().saveDriverData(response["body"]);
+          debugPrint("new user : " +
+              Get.find<UserController>().passenger.value.toString());
+          Get.to(() => SelectingVechicleTypeScreen());
+        }
+      }
+      return;
     }
   }
 }
--- driver_app/lib/controllers/user_controller.dart
+++ driver_app/lib/controllers/user_controller.dart
@@ -1,3 +1,27 @@
+import 'package:driver_app/modals/driver.dart';
+import 'package:driver_app/modals/vehicle.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class UserController extends GetxController {}
+class UserController extends GetxController {
+  var passenger = Driver(vehicle: Vehicle()).obs;
+
+  void saveDriverData(dynamic data) {
+    passenger.update((val) {
+      val!.id = data["id"];
+      val.phone = data["phone"];
+      val.email = data["email"] == null ? "" : data["email"];
+      val.name = data["name"] == null ? "" : data["email"];
+      val.totalRating = data["totalRating"];
+      val.totalRides = data["totalRides"];
+      val.pastRides = List<String>.from(data["pastRides"]);
+      val.token = data["token"];
+      val.refreshToken = data["refreshToken"];
+      val.enabled = data["enabled"];
+      val.suspend = data["suspend"] == null ? false : data["suspend"];
+      val.vehicle = data["vehicle"] == null
+          ? Vehicle()
+          : Vehicle.fromJson(data["vehicle"]);
+    });
+  }
+}
