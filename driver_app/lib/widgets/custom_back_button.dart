9c2f8c6e0bd55e71b7a2c4914f192034b620af98
Avishka <mailavishka@gmail.com>
2021-10-21 18:47:22 +0530

    cuztomize buck button to give onTap method

--- driver_app/lib/widgets/custom_back_button.dart
+++ driver_app/lib/widgets/custom_back_button.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:driver_app/theme/colors.dart';
 
 class CustomBackButton extends StatelessWidget {
+  final onTap;
   const CustomBackButton({
     Key? key,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.of(context).pop();
-      },
+      onTap: onTap == null
+          ? () {
+              Navigator.of(context).pop();
+            }
+          : onTap,
       child: Container(
         width: 45,
         height: 45,
