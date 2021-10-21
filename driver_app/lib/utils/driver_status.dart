656a582f90632a6897cabd408eade48370277ac2
Avishka <mailavishka@gmail.com>
2021-10-21 18:49:46 +0530

    add util methods to enums

--- driver_app/lib/utils/driver_status.dart
+++ driver_app/lib/utils/driver_status.dart
@@ -2,3 +2,19 @@ enum DriverState {
   ONLINE,
   OFFLINE,
 }
+
+DriverState getDriverState(String topic) {
+  if (topic == "ONLINE") {
+    return DriverState.ONLINE;
+  } else {
+    return DriverState.OFFLINE;
+  }
+}
+
+String getDriverStateString(DriverState topic) {
+  if (topic == DriverState.ONLINE) {
+    return "ONLINE";
+  } else {
+    return "OFFLINE";
+  }
+}
