511b4b7a8e9a02747026ceaedb840332e1b234e6
Avishka <mailavishka@gmail.com>
2021-10-23 09:11:40 +0530

    adding ride related apis

--- driver_app/lib/api/driver_ride_api.dart
+++ driver_app/lib/api/driver_ride_api.dart
@@ -1,7 +1,10 @@
 import 'package:driver_app/api/utils.dart';
 
-Future<dynamic> toggleStatus(
-    {required double x, required double y, required String token}) async {
+Future<dynamic> toggleStatus({
+  required double x,
+  required double y,
+  required String token,
+}) async {
   String url = '/api/driver/toggleStatus';
   dynamic response = await putRequest(
     url: url,
@@ -10,6 +13,87 @@ Future<dynamic> toggleStatus(
         "x": x,
         "y": y,
       }
+    },
+    token: token,
+  );
+  return response;
+}
+
+Future<dynamic> accept({
+  required String id,
+  required String token,
+}) async {
+  String url = '/api/driver/ride/accept';
+  dynamic response = await putRequest(
+    url: url,
+    data: {
+      "id": id,
+    },
+    token: token,
+  );
+  return response;
+}
+
+Future<dynamic> arrived({
+  required String id,
+  required String token,
+}) async {
+  String url = '/api/driver/ride/status/arrived';
+  dynamic response = await postRequest(
+    url: url,
+    data: {
+      "id": id,
+    },
+    token: token,
+  );
+  return response;
+}
+
+Future<dynamic> picked({
+  required String id,
+  required String token,
+}) async {
+  String url = '/api/driver/ride/status/picked';
+  dynamic response = await postRequest(
+    url: url,
+    data: {
+      "id": id,
+    },
+    token: token,
+  );
+  return response;
+}
+
+Future<dynamic> dropped({
+  required String id,
+  required String token,
+}) async {
+  String url = '/api/driver/ride/status/dropped';
+  dynamic response = await postRequest(
+    url: url,
+    data: {
+      "id": id,
+    },
+    token: token,
+  );
+  return response;
+}
+
+Future<dynamic> finished({
+  required String id,
+  required String passengerFeedback,
+  required int driverRating,
+  required int waitingTime,
+  required String token,
+}) async {
+  String url = '/api/driver/ride/status/finished';
+  dynamic response = await postRequest(
+    url: url,
+    data: {
+      "id": id,
+      "passengerFeedback": passengerFeedback,
+      "driverRating": driverRating,
+      "waitingTime": waitingTime,
     },
     token: token,
   );
